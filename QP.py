edb8beeab2525504a56bb9173448101c3c8e3f18
HarshilBhatia <61454187+HarshilBhatia@users.noreply.github.com>
2020-10-02 23:17:28 +0530

    QP solver

---
+++ QP.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+#import pyomo 
+
+
+# getting values of Returns and Sigma 
+
+r = pd.read_excel('Returns.xlsx')
+s = pd.read_excel('covariance1.xlsx')
+
+returns = r['Return']
+#print(returns)
+sigma = s.loc[:,s.columns!='INDEX'];
+sigma = sigma*100*100;
+returns = returns*100;
+sigma = sigma.to_numpy();
+
+import localsolver
+
+with localsolver.LocalSolver() as ls:
+    # Declares the optimization model
+    model = ls.model
+
+    N = 25;
+    n =12;
+        
+    x = [model.bool() for i in range(N)]
+
+    # weight constraint
+    
+    min_sigx = 0;
+
+    for i in range(N):
+        min_sigx+=x[i]*sigma[i][i]
+
+    for i in range(N):
+        for j in range(i+1,N):
+            min_sigx += x[i]*x[j]*sigma[i][j];
+    
+    #summation xi
+    xi =0 ;
+    for i in x:
+        xi+=i;
+    model.constraint(xi == n)
+
+    R =  450;
+    # Initilize Expected Return to 0 for now . 
+
+    # myu * x 
+    ret = 0; 
+    for i in range(N):
+        ret += returns[i]*x[i];
+
+    model.constraint(ret>=R)
+    # maximize value
+
+    
+    model.minimize(min_sigx);
+    model.close()
+    #
+    # Parameterizes the solver
+    #
+    ls.param.time_limit = 10
+
+    ls.solve()
+   # ls.solution.get_value(x)
+    #print(min_sigx)
+
+    print(ls.solution.get_value(min_sigx))
+    print(ls.solution.get_value(ret))
+    
+    #print(ls.solution.get_value(xi))
+    print()
+    s = 0;
+    for i in range(N):
+        if(x[i].value==1):
+            s += returns[i]
+            print(i)
