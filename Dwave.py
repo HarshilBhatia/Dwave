ead133927d2d826da321ac1e1606b7f73cd00497
HarshilBhatia <harshilbhatia2001@gmail.com>
2020-10-02 13:19:21 +0530

    Comments Added

--- Dwave.py
+++ Dwave.py
@@ -4,28 +4,33 @@ import pandas as pd
 from pyqubo import Array, Placeholder, solve_qubo, Constraint
 from pyqubo import Sum, Model, Mul
 
+
+# getting values of Returns and Sigma 
+
 r = pd.read_excel('Returns.xlsx')
 s = pd.read_excel('covariance1.xlsx')
+
 returns = r['Return']
 #print(returns)
 sigma = s.loc[:,s.columns!='INDEX'];
 sigma = sigma*100*100;
-sigma = sigma.to_numpy();
 returns = returns*100;
+sigma = sigma.to_numpy();
+
+
 
 N = 25;
 n = 12;
 
 x = Array.create('vector', N, 'BINARY')
 #print(x)
+
 # Constraints in our model
 
 #x' Sigmax
 min_sigx = 0;
-# Defining constraints in the Model
-#H  = sigma[0][0] * x[vector[0]];
-min_sigx = 0;
-H =0;
+
+H =0;#temp hamiltonian for the Constraint
 for i in range(N):
     H+=x[i]*sigma[i][i]
 
@@ -35,7 +40,7 @@ for i in range(N):
 
 min_sigx += Constraint(H,label = "min_portfolio");
 
-H =0 ;
+H =0 ;#temp hamiltonian for the Constraint
 for i in x:
     H+=i;
 H-=n;
@@ -43,8 +48,9 @@ H = H**2;
 
 select_n = Constraint(H , label = "select_n_projects")
 
-ExpectedReturn = 0 ;
-H = 0;
+ExpectedReturn = 0 ; # Initilize Expected Return to 0 for now . 
+
+H = 0; #temp hamiltonian for the Constraint
 for i in range(N):
     H += returns[i]*x[i];
 
@@ -67,13 +73,14 @@ qubo, offset = model.to_qubo()
 useQPU = False;
 Cs = 420;
 
+# This is exactly same as your code , I haven't yet changed anything in this regards
+
 if useQPU:
     sampler = EmbeddingComposite(DWaveSampler())
     response = sampler.sample_qubo(qubo, chain_strength=Cs, num_reads =1000) #solver=DWaveSampler()) #, num_reads=50)
 else:
     sampler = neal.SimulatedAnnealingSampler()
     response = sampler.sample_qubo(qubo, num_sweeps=10000, num_reads=50)
-# Postprocess solution
 
 sample = response.first.sample
 
