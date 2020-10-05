1c93d17bb47007bcc2cdf775bb771cc9bd90b300
Harshil Bhatia <harshilbhatia2001@gmail.com>
2020-10-05 19:30:42 +0530

    All energy iteration

--- Dwave.py
+++ Dwave.py
@@ -16,8 +16,8 @@ sigma = sigma.to_numpy();
 
 
 
-N = 25;
-n = 1;
+N = 15;
+n = 8;
 mx = 0;
 for i in returns:
     mx=max(i,mx)
@@ -30,13 +30,10 @@ x = Array.create('arr', N+K, 'BINARY')
 #print(x)
 
 # Constraints in our model
-print(K)
 #x' Sigmax
 min_sigx = 0;
 
 H =0;#temp hamiltonian for the Constraint
-for i in range(N):
-    H+=x[i]*sigma[i][i]
 
 for i in range(N):
     for j in range(N):
@@ -53,63 +50,105 @@ H = H**2;
 #print(x)
 select_n = Constraint(H , label = "select_n_projects")
 #print(select_n)
-ExpectedReturn = 0 ; # Initilize Expected Return to 0 for now . 
+R = 220 ; # Initilize Expected Return to 0 for now . 
 
 H = 0; #temp hamiltonian for the Constraint
 for i in range(N):
     H += returns[i]*x[i];
 
-H -= ExpectedReturn
+H -= R
 
-for i in range(0,K-N):
+for i in range(0,K):
     H-= (2**i)*(x[i+N]);
 
 H = H**2;
 
 expR = Constraint(H, label = "min_expected_return");
 
-lambda1 = 100000;
-lambda2 = 1;
-
-H = min_sigx + lambda1*select_n + lambda2*expR;
-
-model = H.compile();
-qubo, offset = model.to_qubo()
-
-#print(qubo)
-
-useQPU = False;
-Cs = 420;
-
-# This is exactly same as your code , I haven't yet changed anything in this regards
-
-if useQPU:
-    sampler = EmbeddingComposite(DWaveSampler())
-    response = sampler.sample_qubo(qubo, chain_strength=Cs, num_reads =1000) #solver=DWaveSampler()) #, num_reads=50)
-else:
-    sampler = neal.SimulatedAnnealingSampler()
-    response = sampler.sample_qubo(qubo, num_sweeps=10000, num_reads=50)
-
-sample = response.first.sample
-
-final_sigx=0;
-
+mx=0;
+mn=1e9;
 for i in range(N):
-    for j in range(N):
-        final_sigx += sample['arr[{}]'.format(i)]*sample['arr[{}]'.format(j)]*sigma[i][j];
-final_return=0;
+    mx = max(mx,returns[i]);
+    mn = min(mn,returns[i]);
 
-for i in range(N):
-    final_return += returns[i]*sample['arr[{}]'.format(i)];
+for i in range (1,2):
+    sl = 0;
+    lambda1 = (mx-mn);
+    lambda1 *= 10;
+    #print(lambda1)
+    lambda2 = 1;
+    #mul = 20;
+    H = min_sigx + lambda1*select_n + lambda2*expR;
 
-#print obj and final return 
-print(final_sigx,final_return)
+    model = H.compile();
+    qubo, offset = model.to_qubo()
+
+    #print(qubo)
+
+    useQPU = False;
+    Cs = 420;
+
+    # This is exactly same as your code , I haven't yet changed anything in this regards
+
+    if useQPU:
+        sampler = EmbeddingComposite(DWaveSampler())
+        response = sampler.sample_qubo(qubo, chain_strength=Cs, num_reads =1000) #solver=DWaveSampler()) #, num_reads=50)
+    else:
+        sampler = neal.SimulatedAnnealingSampler()
+        response = sampler.sample_qubo(qubo, num_sweeps=10000, num_reads=1000)
+
+    #sample = response.first.sample
+    
+    
+    """
+    final_sigx=0;
+
+    for i in range(N):
+        for j in range(N):
+            final_sigx += sample['arr[{}]'.format(i)]*sample['arr[{}]'.format(j)]*sigma[i][j];
+    final_return=0;
+
+    for i in range(N):
+        final_return += returns[i]*sample['arr[{}]'.format(i)];
 
 
-# print selected values
-for i in range (N):
-    if(sample['arr[{}]'.format(i)]==1):
-        print(i)
+    print(final_sigx,final_return)
+    """
 
+    x=response.first.sample
+    mn=1e9;
 
-#Sum(0, K, lambda i: var[N + j*K +i]*(2**(i))) - v[j])**2
+    for sample in response.samples():   
+        final_sigx=0; 
+        n_temp=0;
+        for i in range(N):
+            n_temp += sample['arr[{}]'.format(i)]
+        for i in range(N):
+            for j in range(N):
+                final_sigx += sample['arr[{}]'.format(i)]*sample['arr[{}]'.format(j)]*sigma[i][j];
+        ret = 0;
+        for i in range(N):
+            ret += sample['arr[{}]'.format(i)] *returns[i];
+        #print(final_sigx,n_temp)
+
+        if(final_sigx<mn and n_temp == n ):
+            mn=final_sigx;
+            x = sample;
+            final_ret = ret;
+        
+        ''' print(mn)
+        #print(x)
+        print(final_ret)
+        '''
+        
+   # print(x)
+    #print 'best - optimal' solution obtained
+    for i in range(N):
+        if(x['arr[{}]'.format(i)]==1):
+            print(i)
+
+    print(mn)
+    print(final_ret)
+    
+    
+        #Sum(0, K, lambda i: var[N + j*K +i]*(2**(i))) - v[j])**2
