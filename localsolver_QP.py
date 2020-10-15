e5bcbaeafc8ef8fcfc54ae1803b04efdb5d5da2d
Harshil Bhatia <harshilbhatia2001@gmail.com>
2020-10-15 13:28:44 +0530

    LocalSolver

--- localsolver_QP.py
+++ localsolver_QP.py
@@ -5,35 +5,33 @@ import pandas as pd
 
 # getting values of Returns and Sigma 
 
-r = pd.read_excel('Returns.xlsx')
-s = pd.read_excel('covariance1.xlsx')
+r = pd.read_excel('returnsall.xlsx')
+s = pd.read_excel('covariances.xlsx')
 
 returns = r['Return']
 #print(returns)
 sigma = s.loc[:,s.columns!='INDEX'];
 #sigma = sigma*100*100;
-#eturns = returns*100;
+#eturns = returns*100;  
 sigma = sigma.to_numpy();
+returns = abs(returns);
 
-"""
-for i in range(25):
-    for j in range(25):
-        sigma[i][j]=100000000;
+sigma = (sigma).astype(int);
+returns =(returns*1).astype(int);
 
-sigma[0][0]=1;
-sigma[1][1]=1;
-"""
 
+# print(returns.shape)
+# print(sigma.shape)
 
 import localsolver
 
 with localsolver.LocalSolver() as ls:
     # Declares the optimization model
     model = ls.model    
-
+    #updated the code.
     N = 25;
-    n =12;
-        
+    n = 5;
+    R = 0;
     x = [model.bool() for i in range(N)]
 
     # weight constraint
@@ -45,14 +43,12 @@ with localsolver.LocalSolver() as ls:
             min_sigx += x[i]*x[j]*sigma[i][j];
         
 
-
     #summation xi
     xi =0 ;
     for i in x:
         xi+=i;
     model.constraint(xi == n)
 
-    R =  450;
     # Initilize Expected Return to 0 for now . 
 
     # myu * x 
@@ -72,11 +68,14 @@ with localsolver.LocalSolver() as ls:
     ls.param.time_limit = 10
 
     ls.solve()
-   # ls.solution.get_value(x)
+# ls.solution.get_value(x)
     #print(min_sigx)
-
-    print(ls.solution.get_value(min_sigx))
-    print(ls.solution.get_value(ret))
+    #print(ls.solution.get_objective_bound(1))
+    #print(ls.solution.get_objective_bound(2))
+    
+    print();
+    print("x'Sx = ", ls.solution.get_value(min_sigx))
+    print("Returns = ",ls.solution.get_value(ret))
     
     #print(ls.solution.get_value(xi))
     print()
